e717253f490232fe955441dc7ea63f4616f93fa1
Ken.Zhang <better.man@aliyun.com>
2020-05-29 10:29:49 +0800

    test(淘宝SDK): client单元测试
    新增client单元测试代码

--- client_test.go
+++ client_test.go
@@ -1,1 +1,29 @@
 package opentaobao_test
+
+import (
+	"log"
+	"testing"
+
+	opentaobao "github.com/beJuvenile/go-sdk-taobao"
+	"github.com/beJuvenile/go-sdk-taobao/requests/tbk"
+)
+
+func TestClient(t *testing.T) {
+	c := opentaobao.New()
+	c.AppKey = "2526218"
+	c.AppSecret = "da2e7dd98976df40fae3899afab4bfe"
+	req := tbk.OrderDetailsGetRequest()
+	req.SetStartTime("2020-05-27 11:00:00")
+	req.SetEndTime("2020-05-27 18:00:00")
+	req.SetOrderScene(2)
+	body, err := c.Exec(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var result tbk.OrderDetailsGetData
+	result, err = tbk.OrderDetailsGetResult(body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(result.Results)
+}
